4c932de669d6702649882b10af6acfaa4282d8ab
MohithKumar85 <mohithkumar657@gmail.com>
2026-03-13 20:27:01 +0530

    Add user authentication with JWT support, including signup and login views. Updated settings and URLs to integrate Django REST Framework and JWT authentication. Created user models for signup and login functionality.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Signup:
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    confirm_password = models.CharField(max_length=100)
+    phone = models.int(max_length=10)
+
+class Login:
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
