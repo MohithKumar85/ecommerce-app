b0c203fe2f3dbffee0196f618ac332f0ccc0e582
MohithKumar85 <mohithkumar657@gmail.com>
2026-03-13 20:37:41 +0530

    Refactor user models to inherit from Django's models.Model. Updated phone field type in Signup model to CharField. Added initial migration for user authentication models.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-class Signup:
+
+class Signup(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     confirm_password = models.CharField(max_length=100)
-    phone = models.int(max_length=10)
+    phone = models.CharField(max_length=10)
 
-class Login:
+
+class Login(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
